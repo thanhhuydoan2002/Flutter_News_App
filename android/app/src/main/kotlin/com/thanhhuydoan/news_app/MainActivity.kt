1678a51108b297201dab2e8f90becdc698cf4d09
thanhhuydoan2002 <ddoanthanhhuy@gmail.com>
2023-09-30 13:28:01 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/thanhhuydoan/news_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.thanhhuydoan.news_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:"Flutter News App",
+      theme: ThemeData.light(),
+      home: const App(),
+    );
+  }
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Hello world !!"),
+      ),
+    );
+  }
+}
